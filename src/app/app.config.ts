4bc1feab8e00e8c801259623a527523f2cb33a02
VIKASHNAGAR786 <vikashnagar872@gmail.com>
2025-05-24 16:51:42 +0530

    Refactor routing and configuration for Angular application
    - Removed legacy web.config files from docs and public directories.
    - Added new routes.json file to define routing for the application.
    - Updated app.config.ts to include necessary providers for animations, HTTP client, and HashLocationStrategy.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 };
